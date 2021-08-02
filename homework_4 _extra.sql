9f67789f7935259adac8c48b5a2238fec9482343
NigistaSaba <nigistasaba@gmail.com>
2021-08-02 19:55:58 +0300

    Update homework_4 _extra.sql

--- homework_4 _extra.sql
+++ homework_4 _extra.sql
@@ -1,9 +1,10 @@
 CREATE TABLE likes (user_id INT , type_id INT, link VARCHAR(255));
 INSERT INTO likes VALUES
-	('1', '2', 'https://vk.com/feed?w=wall-110858602_16368');
-CREATE TABLE types (name VARCHAR(20));
+	('1', '2', 'https://vk.com/feed?w=wall-110858602_16368'); 
+
+CREATE TABLE types (id INT, name VARCHAR(20));
 INSERT INTO  types VALUES
-	('post'),
-	('video'),
-	('audio'),
-	('photo');
+	('1', 'post'),
+	('2', 'video'),
+	('3' ,'audio'),
+	('4','photo');
